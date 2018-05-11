95fc13af071bdb03eb07080440446dfdcd5db4b6
Daniel Kapper <dkapper01@gmail.com>
2018-05-10 23:17:03 -0400

    connecting db in app file

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var mongoose = connect('mongodb://dkapper01:webscraper@ds119650.mlab.com:19650/web-scraper');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
